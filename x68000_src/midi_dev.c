bf50dcda55a7926243806e286d5d1c0678614bc2
Florian Penzkofer <fp@nullptr.de>
2025-04-07 21:48:27 -0500

    Midiori Synth commands

--- x68000_src/midi_dev.c
+++ x68000_src/midi_dev.c
@@ -23,9 +23,23 @@ static void sigbus_handler(int sig) {
 	siglongjmp(midi_init_env, 1);
 }
 
+#define MIDIORI_CMD_COUNT 16
+static unsigned char midiori_command[MIDIORI_CMD_COUNT];
+static unsigned char midiori_command_has_data2[MIDIORI_CMD_COUNT];
+static unsigned char midiori_data[MIDIORI_CMD_COUNT];
+static unsigned char midiori_data2[MIDIORI_CMD_COUNT];
+static unsigned char midiori_count;
+
+static void send_midiori_command(int i)
+{
+	unsigned char buf[] = {0xF0, 0x7D, midiori_command[i], midiori_data[i], midiori_command_has_data2[i] ? midiori_data2[i] : 0xF7, 0xF7};
+	mididev_send_bytes(buf, midiori_command_has_data2[i] ? sizeof(buf) : sizeof(buf)-1);
+	//printf("send_midiori_command %x %x %x %x %x\n", (int)buf[0], (int)buf[1], (int)buf[2], (int)buf[3], (int)buf[4]);
+}
+
 int mididev_init(void) {
 	struct sigaction sa;
-	int version;
+	int version, i;
 	version = mcdrv_version();
 	if(version >= 0x500000)
 		midi_driver_type = MIDI_DRIVER_MCDRV;
@@ -43,6 +57,11 @@ int mididev_init(void) {
 			/* hardware not installed */
 			return -1;
 		}
+	}
+	
+	for (i = 0; i < midiori_count; ++i)
+	{
+		send_midiori_command(i);
 	}
 	
 	return 0;
@@ -75,12 +94,86 @@ int mididev_send_bytes(const unsigned char *buf, int len) {
 }
 
 void mididev_print_usage(void) {
+	printf("-z vol=[0-255]: Volume of the selected synth.\n");
+	printf("-z sel=[0 or 1]: Select active synth (0=MT32-Pi 1=Wavetable).\n");
+	printf("-z fm=[0-255/0-255]: Volume of the X68000's FM/PCM audio.\n");
+	printf("-z pi=[0-255/0-255]: Volume of the MT32-Pi.\n");
+	printf("-z wt=[0-255/0-255]: Volume of the wavetable board.\n");
+	printf("-z lr=[0 or 1]: Swap stereo channels of wavetable board\n");
+	printf("-z int=[0-255/0-255]: Volume of external output.\n");
+	printf("-z ext=[0-255/0-255]: Volume of internal output.\n");
 }
 
+static char *dev_optstr = "z:";
+
 void mididev_add_optstr(char *optstr) {
+	int main_len = strlen(optstr);
+	strcpy(optstr+main_len, dev_optstr);
+}
+
+static int parse_volume(const char* str) {
+	char* end;
+	long int arg0, arg1;
+	arg0 = strtol(str, &end, 10);
+	midiori_data[midiori_count] = arg0 < 0 ? 0 : arg0 > 255 ? 255 : arg0;
+	if (*end == '/') {
+		arg1 = strtol(end + 1, 0, 10);
+		midiori_data2[midiori_count] = arg0 < 0 ? 0 : arg1 > 255 ? 255 : arg1;
+	} else {
+		midiori_data2[midiori_count] = midiori_data[midiori_count];
+	}
+	midiori_command_has_data2[midiori_count] = 1;
+	++midiori_count;
+	return 0;
 }
 
 int mididev_parse_arg(int c, const char *optarg) {
+	long int arg;
+	if(c=='z') {
+		if (midiori_count >= MIDIORI_CMD_COUNT)
+			return -1;
+
+		midiori_command_has_data2[midiori_count] = 0;
+		if (!strncmp(optarg, "vol=", 4)) {
+			midiori_command[midiori_count] = 0x37;
+			arg = strtol(optarg + 4, 0, 10);
+			midiori_data[midiori_count] = arg < 0 ? 0 : arg > 255 ? 255 : arg;
+			++midiori_count;
+			return 0;
+		}
+		if (!strncmp(optarg, "sel=", 4)) {
+			midiori_command[midiori_count] = 0x36;
+			midiori_data[midiori_count] = strtol(optarg + 4, 0, 10) ? 1 : 0;
+			++midiori_count;
+			return 0;
+		}
+		if (!strncmp(optarg, "lr=", 3)) {
+			midiori_command[midiori_count] = 0x35;
+			midiori_data[midiori_count] = strtol(optarg + 3, 0, 10) ? 1 : 0;
+			++midiori_count;
+			return 0;
+		}
+		if (!strncmp(optarg, "fm=", 3)) {
+			midiori_command[midiori_count] = 0x30;
+			return parse_volume(optarg + 3);
+		}
+		if (!strncmp(optarg, "pi=", 3)) {
+			midiori_command[midiori_count] = 0x31;
+			return parse_volume(optarg + 3);
+		}
+		if (!strncmp(optarg, "wt=", 3)) {
+			midiori_command[midiori_count] = 0x32;
+			return parse_volume(optarg + 3);
+		}
+		if (!strncmp(optarg, "ext=", 4)) {
+			midiori_command[midiori_count] = 0x33;
+			return parse_volume(optarg + 4);
+		}
+		if (!strncmp(optarg, "int=", 4)) {
+			midiori_command[midiori_count] = 0x34;
+			return parse_volume(optarg + 4);
+		}
+		return -1;
+	}
 	return -1;
 }
-
